952567cddc46cea3eecdac2570fb1b3bc7737d3c
Yahaya Kehinde <mac@Yahaya-Kehinde-macbook.local>
2020-11-24 08:34:40 +0100

    bug: fixed add and remove from cart

--- cart_app/urls.py
+++ cart_app/urls.py
@@ -2,7 +2,7 @@ from django.urls import  path
 from cart_app import views
 
 urlpatterns = [
-   path('add_to_cart/<slug>', views.add_to_cart, name = 'add-to-cart'),
-   path('remove_from_cart/<slug>', views.remove_from_cart, name = 'remove_from_cart'),
+   path('home/add_to_cart/<slug>', views.add_to_cart, name = 'add-to-cart'),
+   path('home/remove_from_cart/<slug>', views.remove_from_cart, name = 'remove_from_cart'),
    path('cart/', views.cart, name = "cart")
 ]
