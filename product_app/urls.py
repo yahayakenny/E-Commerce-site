1dfc05bb828825dba9bc206350fafb68fa40e81a
Yahaya Kehinde <mac@Yahaya-Kehinde-macbook.local>
2020-11-24 08:24:15 +0100

    updated styling

--- product_app/urls.py
+++ product_app/urls.py
@@ -2,6 +2,6 @@ from django.urls import  path
 from product_app import views
 
 urlpatterns = [
-     path('', views.home, name = 'home'),
+     path('home/', views.home, name = 'home'),
      path('home/<slug>', views.product_detail, name = 'product_detail')
 ]
